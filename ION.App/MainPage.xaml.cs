acf4b5d3b99ee6ae298303431ed3f6486f6669c5
zylladys <gfernandes1990@gmail.com>
2026-09-09 23:34:29 -0300

    feat: add initial ION dashboard

--- ION.App/MainPage.xaml.cs
+++ ION.App/MainPage.xaml.cs
@@ -2,22 +2,17 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object? sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    [Obsolete]
+    private async void OnAddChannelClicked(object sender, EventArgs e)
+    {
+        await DisplayAlert(
+            "ION",
+            "Channel configuration is coming next.",
+            "OK");
+    }
 }
